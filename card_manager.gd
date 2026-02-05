873ff5a189a82fb1123e414227c5af87b0e03b6f
OliveADay <sharinggon23@gmail.com>
2026-02-05 13:10:03 -0800

    changing to manager really managing

--- card_manager.gd
+++ card_manager.gd
@@ -3,5 +3,11 @@ extends Node2D
 var rand = RandomNumberGenerator.new()
 
 func _ready() -> void:
-	var num = rand.randi_range(1,4)
+	var secret = 1
+	var amount = 0
+	for card in cards:
+		card=secret
+		amount+=1
+		if amount==4:
+			secret+=1
 	
