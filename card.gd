a2590c41b8883aef9e6abcd8388d596c5f72a343
OliveADay <sharinggon23@gmail.com>
2026-02-05 17:44:19 -0800

    small changes to card and positions

--- card.gd
+++ card.gd
@@ -3,18 +3,14 @@ extends Area2D
 var mouseOn = false
 var rand = RandomNumberGenerator.new()
 var secret = 1;
-var maxWait = 5;
+var maxWait = 1;
+var startWait = 5
 var currentWait = 0;
-
-func switch(val, full, list):
-	if not full:
-		secret = val
-	else:
-		secret = rand.randi_range(1,val);
+signal clicked
 		
 func _ready():
 	$Sprite2D.frame = secret
-	currentWait = maxWait
+	currentWait = startWait
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("click") and mouseOn:
@@ -25,7 +21,6 @@ func _process(delta: float) -> void:
 		$Sprite2D.frame = secret
 	else:
 		$Sprite2D.frame = 0
-		
 func _on_mouse_entered() -> void:
 	mouseOn = true
 
